2c371dfdfa2ccd783a13439e127f5514502452ba
mhausenblas <michael.hausenblas@deri.org>
2011-07-28 17:02:28 +0100

    giving up on Channels for now ...

--- turtter-space.py
+++ turtter-space.py
@@ -15,34 +15,44 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 MAIN_SPACE_ID = 'http://turtter-space.appspot.com/main#'
 MAIN_SPACE_TOKEN = ''
 current_turt = {}
-MAIN_SPACE_TOKEN = channel.create_channel(MAIN_SPACE_ID)
-logging.info('SPACE CREATED: %s' %MAIN_SPACE_TOKEN)
+tclients = {}
 
-class TurtterSpacePingHandler(webapp.RequestHandler):
-	def post(self):
-		payload = self.request.get('t')
-		ping_turt = simplejson.loads(payload)
-		client_id = self.request.get('from')
-		logging.info('{%s} USER %s has joined the space ...' %(MAIN_SPACE_TOKEN, client_id))
-		channel.send_message(MAIN_SPACE_TOKEN, payload)
-		
 class TurtterSpaceChatHandler(webapp.RequestHandler):
 	def post(self):
 		payload = self.request.get('t')
+		sender = self.request.get('c')
 		current_turt = simplejson.loads(payload)
-		logging.info('{%s} BROADCASTING %s from user <%s>' %(MAIN_SPACE_TOKEN, current_turt['data']['statement'], current_turt['data']['useruri']))
-		channel.send_message(MAIN_SPACE_TOKEN, payload)
+		logging.info('{%s} BROADCASTING %s from user <%s>:' %(sender, current_turt['data']['statement'], current_turt['data']['useruri']))
+		for tcl in tclients:
+			if tcl == sender: # broadcast to all clients but the sender
+				# logging.info(' not echoing to sender %s' %(tcl))
+				pass
+			else:
+				channel.send_message(tcl, payload)
+				logging.info(' SENDING to client %s' %(tcl))
+
+class TurtterSpaceEchoHandler(webapp.RequestHandler):
+	def post(self):
+		payload = self.request.get('t')
+		sender = self.request.get('c')
+		current_turt = simplejson.loads(payload)
+		logging.info('{%s} ECHOING %s to user <%s>:' %(sender, current_turt['data']['statement'], current_turt['data']['useruri']))
+		channel.send_message(sender, payload)
 
 class SpacePage(webapp.RequestHandler):
 	def get(self):
-		template_values = {'token': MAIN_SPACE_TOKEN }
+		client_id = os.urandom(16).encode('hex')
+		channel_key = channel.create_channel(client_id)
+		tclients[client_id] = channel_key
+		template_values = { 'channel': channel_key, 'client' : client_id }
+		logging.info('{%s} NEW client with ID %s' %(channel_key, client_id))
 		path = os.path.join(os.path.dirname(__file__), 'index.html')
 		self.response.out.write(template.render(path, template_values))
 
 application = webapp.WSGIApplication([
 	('/', SpacePage),
-	('/ping', TurtterSpacePingHandler),
-	('/chat', TurtterSpaceChatHandler)
+	('/chat', TurtterSpaceChatHandler),
+	('/echo', TurtterSpaceEchoHandler)
 	], debug=True)
 
 
